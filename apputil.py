5b22887b29bf76c463f315ce325f31748f69d019
mimitabahizi <mibahizi@iu.edu>
2026-09-23 19:24:31 -0400

    Complete Fibonacci recursion exercise

--- apputil.py
+++ apputil.py
@@ -3,3 +3,11 @@ import pandas as pd
 
 
 # update/add code below ...
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+print(fibonacci(9))
